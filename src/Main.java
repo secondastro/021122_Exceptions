06429cde876d537060162d54d6cf76ec8e701032
User <anton_trenogin@mail.ru>
2022-11-09 23:22:06 +0500

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        User one = new User("", "12345678","12345678");
+        User two = new User("петька", "", "2");
+        User three = new User("Logan", "12345567", "12345678");
+        User four = new User("Lager", "123456", "123456");
+
+        check(one,two,three,four);
+
+
+        }
+    public static void check(User... users) {
+        for (User user : users) {
+            try {
+                User.checkData(user);
+            } catch (WrongLoginException e) {
+                System.out.println(e.getMessage() + " " +  '"' + user.getLogin() + '"');
+
+            } catch (WrongPasswordException e) {
+                System.out.println("пользователь " + user.getLogin() + ": " +  e.getMessage());
+            }
+
+        }
     }
+
 }
